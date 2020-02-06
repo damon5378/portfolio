a155f3c09e5f00dc94ac381ea0c8c78340a3516f
damon5378 <iosifov.konstantin@gmail.com>
2020-02-06 21:07:59 +0300

    Updates

---
+++ precache-manifest.3c5c0875155dc92d1ad62a24b45af167.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ce2877e2881b54663e08f2acf59a8ad",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "518e8efb6163f091864e",
+    "url": "/portfolio/static/css/main.abd33d12.chunk.css"
+  },
+  {
+    "revision": "c9f9821bafe26152f827",
+    "url": "/portfolio/static/js/2.c4db581e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio/static/js/2.c4db581e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "518e8efb6163f091864e",
+    "url": "/portfolio/static/js/main.78dffc0d.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  }
+]);
