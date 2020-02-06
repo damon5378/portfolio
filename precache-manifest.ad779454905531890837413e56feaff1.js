7249bf7c933b6be472a0063758d56563ad50e268
damon5378 <iosifov.konstantin@gmail.com>
2020-02-06 22:25:23 +0300

    skills added-1

---
+++ precache-manifest.ad779454905531890837413e56feaff1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7287439116187c8ae130f4e07218f1d0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "63ecb3424b1a07746fe2",
+    "url": "/portfolio/static/css/main.16397737.chunk.css"
+  },
+  {
+    "revision": "459dadee7b5220abfcca",
+    "url": "/portfolio/static/js/2.574a47f9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio/static/js/2.574a47f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63ecb3424b1a07746fe2",
+    "url": "/portfolio/static/js/main.c03d85dc.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  }
+]);
