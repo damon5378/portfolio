76183ca948cdf8a1f2e29132825cef44d81a887e
beginner-frontender <44082329+damon5378@users.noreply.github.com>
2020-02-06 15:15:30 +0300

    Add files via upload

---
+++ precache-manifest.540a55f9246ce86ebdd4602aae3ad559.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f75c6a8fa251aba6e3b575a9902b58d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a081df8669ef5f0f179c",
+    "url": "/static/css/main.1d3a25c7.chunk.css"
+  },
+  {
+    "revision": "c9f9821bafe26152f827",
+    "url": "/static/js/2.c4db581e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.c4db581e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a081df8669ef5f0f179c",
+    "url": "/static/js/main.a9629db1.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  }
+]);
